3b87ad705b47119eeb940c3ab1f6735677048227
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2022-11-28 19:58:26 -0500

    added variables to .js

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,36 @@
-// Assignment code here
+// Assignment code heref
+
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+
+   var isUppercase = confirm("Would you like to use uppercase ?");
+   var isLowercase = confirm("Would you like to use lowercase ?");
+   var isSpecial = confirm("Would you like to use special chatacter ?");
+   var isNumber = confirm("Would you like to use numbers ?");
+
+   if (isUppercase) {
+    //use uppercase letters
+    console.log("uppercase");
+   }
+
+   if (isLowercase) {
+    //use lowercase letters
+     console.log("lowercase");
+   }
+   if (isSpecial) {
+    //use special characters
+      console.log("special");
+   }
+   if (isNumber) {
+    //use numbers
+      console.log("number");
+   }
+  return "";
+}
 
 // Write password to the #password input
 function writePassword() {
