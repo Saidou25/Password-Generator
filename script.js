b454de494d2f3527cf0f017a44821678e84ada4e
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2022-12-01 19:04:19 -0500

    cleaned up the code and completed the character length others outcomes

--- script.js
+++ script.js
@@ -1,120 +1,61 @@
-// Assignment code heref
-
-/*
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
+  console.log("Please choose a number between 8 and 128 for the length of your password.")
+  var length = parseInt(prompt("Please choose a number between 8 and 128 for the length of your password."));
+  
+  if (!length || length < 8 || length > 128) {
+alert("Your number has to be berween 8 and 9. Please try again.")
+    generatePassword();
+    return "";
+   } 
 
-*/
-
-/*
   var isUppercase = confirm("Would you like to use uppercase ?");
-   var isLowercase = confirm("Would you like to use lowercase ?");
-   var isSpecial = confirm("Would you like to use special chatacter ?");
-   var isNumber = confirm("Would you like to use numbers ?");
-   
-   
-   var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-   var lowercase = "abcdefghijklmnopqrstuvwxyz";
-   var number = "0123456789";
-   var special = "#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+  var isLowercase = confirm("Would you like to use lowercase ?");
+  var isSpecial = confirm("Would you like to use special chatacter ?");
+  var isNumber = confirm("Would you like to use numbers ?");
 
-}
 
-*/
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var number = "0123456789";
+  var special = "#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
-/*
-   if (isUppercase) {
+  if (isUppercase) {
     //use uppercase letters
     console.log("uppercase");
-    
-   }
-
-   if (isLowercase) {
-    //use lowercase letters
-     console.log("lowercase");
-     
-   }
-   
-   if (isSpecial) {
-    //use special characters
-      console.log("special");
-     
-   }
-   if (isNumber) {
-    //use numbers
-      console.log("number");
-      
-   }
-
-
-*/
-/*
- var length = prompt("Choose the number of charecters you would like for the length of your password.");
- for (length = 8; length < 128; length++) {
- createrandomstring(length)
-  }
- if (length < 8) {
-     console.log("must be between 8 and 128 characters.");
-   } else {
-    createrandomstring(length)
-   }
-   
-  */
-  /*else {
-     console.log("You choosed " + length + " characters for your password.");
-   }  if (length = 0) {
-     console.log("you must enter a number.")
-   }
- */
-  var length = prompt("Choose the number of charecters you would like for the length of your password.");
   
-  if (length < 8) {
-    var length = prompt("choose again")
-   } 
- 
-
-// I need to connect the dots here. program needs to return to prompt window over and over until
-// 7 < length < 128
-
-
-
-
-   var randomstring = "";
-   createrandomstring(length)
-function createrandomstring(stringlength) { 
-  var possCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-  for(var i, i =0; i < stringlength; i++) {
-    randomstring += possCharacters.charAt(Math.floor(Math.random() * possCharacters.length))
-    }
-}
-alert(randomstring);
-
-
-
-
- 
-
-/*
-   
-   var length = prompt("Choose the number of charecters you would like for the length of your password.");
-  if (length < 8) {
-    console.log("must be between 8 and 128 characters.");
-  } else if (length > 128) {
-    console.log("must be between 8 and 128 characters.");
-  } else {
-    console.log("You choosed " + length + " characters for your password.");
-  }  if (length = 0) {
-    console.log("you must enter a number.")
   }
+  
+  if (isLowercase) {
+    //use lowercase letters
+    console.log("lowercase");
+  
+  }
+  
+  if (isSpecial) {
+    //use special characters
+    console.log("special");
+  
+  }
+  if (isNumber) {
+    //use numbers
+    console.log("number");
 
-*/
+    var randomstring = "";
+createrandomstring(length)
+function createrandomstring(stringlength) {
+  var possCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  for (var i, i = 0; i < stringlength; i++) {
+    randomstring += possCharacters.charAt(Math.floor(Math.random() * possCharacters.length))
+  }
+}
+ }
+ alert(randomstring);
+  
+}
 
-
-
- /* 
 
 // Write password to the #password input
 function writePassword() {
@@ -127,7 +68,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-}
-
-*/
