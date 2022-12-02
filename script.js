41acc77b9de26b60eae813d4384e25aee027917a
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2022-12-01 22:36:54 -0500

    added comments

--- script.js
+++ script.js
@@ -4,24 +4,28 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   console.log("Please choose a number between 8 and 128 for the length of your password.")
   var length = parseInt(prompt("Please choose a number between 8 and 128 for the length of your password."));
-  
+  //------------choosing the lengh of the password------------------
   if (!length || length < 8 || length > 128) {
 alert("Your number has to be berween 8 and 128. Please try again.")
     generatePassword();
     return "";
    } 
+//-----------------------------------------------------------------
 
+//-------------------list of needed variables----------------------
   var isUppercase = confirm("Would you like to use uppercase ?");
   var isLowercase = confirm("Would you like to use lowercase ?");
   var isSpecial = confirm("Would you like to use special chatacter ?");
   var isNumber = confirm("Would you like to use numbers ?");
-  var possCharacters = ""
-
+  var possCharacters = ""//this is the input from the user choosing his/her desired character types for the password
+  var randomstring = "";//variable used to establish a random string for the password
   var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var lowercase = "abcdefghijklmnopqrstuvwxyz";
   var number = "0123456789";
-  var special = "#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+  var special = "#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+//------------------------------------------------------------------
 
+//-------------------building up possCharacters by concatenating user's input
   if (isUppercase) {
     console.log("uppercase");
     possCharacters += uppercase;
@@ -44,34 +48,31 @@ alert("Your number has to be berween 8 and 128. Please try again.")
   }
 
   if ( isLowercase || isUppercase || isSpecial || isNumber) {
-
+    console.log("Characters you choosed.", possCharacters);
   } else {
     alert("Please choose at least one type of character.");
     return "";
   }
-  
-console.log(possCharacters);
+//-----------------------------------------------
 
-    var randomstring = "";
-createrandomstring(length)
-function createrandomstring(stringlength) {
+//-------------------genarating random password----------------
+
+   createrandomstring(length)
+   function createrandomstring(stringlength) {
   
-  for (var i, i = 0; i < stringlength; i++) {
+   for (var i, i = 0; i < stringlength; i++) {
     randomstring += possCharacters.charAt(Math.floor(Math.random() * possCharacters.length))
  } 
 }  alert(randomstring);
+
+   return ""; // avoids "undefined" message after delivery of the password
  }
  
- 
-
-// Write password to the #password input
-function writePassword() {
+  function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
-
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
